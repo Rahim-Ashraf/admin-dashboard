bce34e6a7823ebfe62caca6dce36726a8ef66836
unknown <rahim11500000@gmail.com>
2025-02-06 23:11:05 +0600

    added userDetails modal

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { Box, Modal } from "@mui/material";
 import { ChangeEvent, useEffect, useState } from "react";
 
 interface UserType {
@@ -75,8 +76,56 @@ function Users() {
         }
     }
 
+    //for use details modal
+    const [singleUser, setSingleUser] = useState<UserType>()
+    const [open, setOpen] = useState(false);
+    const handleOpen = (user: UserType) => {
+        setSingleUser(user)
+        setOpen(true)
+        console.log(user)
+    }
+    const handleClose = () => setOpen(false);
+
     return (
         <div>
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                    <div className="space-y-2 lg:space-y-4 grid grid-cols-1 lg:grid-cols-2 gap-4">
+                        <div className="w-full">
+                            <h2 className="text-lg lg:text-xl font-semibold">Personal details</h2>
+                            <h3><span className="font-semibold">Name</span>: {singleUser?.name}</h3>
+                            <h3><span className="font-semibold">UserName</span>: {singleUser?.username}</h3>
+                            <h3><span className="font-semibold">Email</span>: {singleUser?.email}</h3>
+                            <h3><span className="font-semibold">Phone</span>: {singleUser?.phone}</h3>
+                            <h3><span className="font-semibold">Websitet</span>: {singleUser?.website}</h3>
+                        </div>
+                        <div className="w-full">
+                            <h2 className="text-lg lg:text-xl font-semibold">Company details</h2>
+                            <h3><span className="font-semibold">Company name</span>: {singleUser?.company.name}</h3>
+                            <h3><span className="font-semibold">Company BS</span>: {singleUser?.company.bs}</h3>
+                            <h3><span className="font-semibold">Catch phrase</span>: {singleUser?.company.catchPhrase}</h3>
+                        </div>
+                        <div className="w-full">
+                            <h2 className="text-lg lg:text-xl font-semibold">Full address</h2>
+                            <h3><span className="font-semibold">City</span>: {singleUser?.address.city}</h3>
+                            <h3><span className="font-semibold">Zipcode</span>: {singleUser?.address.zipcode}</h3>
+                            <h3><span className="font-semibold">Street</span>: {singleUser?.address.street}</h3>
+                            <h3><span className="font-semibold">Suite</span>: {singleUser?.address.suite}</h3>
+                            <h3><span className="font-semibold">GEO-lat</span>: {singleUser?.address.geo.lat}</h3>
+                            <h3><span className="font-semibold">GEO-lng</span>: {singleUser?.address.geo.lng}</h3>
+                        </div>
+                    </div>
+                    <div className="flex justify-center my-4">
+                        <button onClick={handleClose}
+                            className="w-full lg:w-fit bg-slate-600 text-white px-4 py-2 rounded">Close</button>
+                    </div>
+                </Box>
+            </Modal>
             <div className="lg:flex justify-evenly items-center mb-8 mt-4 space-y-4">
                 <div>
                     <label htmlFor="">
@@ -101,7 +150,8 @@ function Users() {
                             <h4><span className="font-semibold">Email:</span> {user.email}</h4>
                             <h4><span className="font-semibold">City:</span> {user.address.city}</h4>
                         </div>
-                        <button className="bg-sky-500 text-white px-4 py-2 rounded mt-4">View details</button>
+                        <button onClick={() => handleOpen(user)}
+                            className="bg-sky-500 text-white px-4 py-2 rounded mt-4">View details</button>
                     </div>
                 ))}
             </div>
@@ -110,3 +160,16 @@ function Users() {
 }
 
 export default Users
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    maxWidth: 1080,
+    minWidth: 320,
+    bgcolor: 'background.paper',
+    boxShadow: 24,
+    borderRadius: 2,
+    p: 2,
+};
